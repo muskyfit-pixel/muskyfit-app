80a58443e4905654755c9f313acfe5ceb0cbb9be
Mitesh Mistry <muskyfit@gmail.com>
2026-02-06 16:12:33 +0000

    Hard reset app shell and router (mobile fix)

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
-import { Routes, Route } from "react-router-dom";
-import MemberDatabase from "./pages/MemberDatabase";
-import Plan from "./pages/Plan";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CoachPortal from "./pages/CoachPortal";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MemberDatabase />} />
-      <Route path="/plan" element={<Plan />} />
-    </Routes>
+    <BrowserRouter>
+      <div
+        style={{
+          minHeight: "100vh",
+          width: "100%",
+          maxWidth: 420,
+          margin: "0 auto",
+          padding: 16,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<CoachPortal />} />
+          <Route path="/coach" element={<CoachPortal />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
