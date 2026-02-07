c54085cb8f2ca9e79a0a24b0af5ef4706a317b7b
Mitesh Mistry <muskyfit@gmail.com>
2026-02-07 23:42:40 +0000

    Fix router nesting and restore app render

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Login from "./pages/Login";
 import { User } from "./types";
@@ -6,20 +6,14 @@ import { User } from "./types";
 function App() {
   const [user, setUser] = useState<User | null>(null);
 
-  const handleLogin = (loggedInUser: User) => {
-    setUser(loggedInUser);
-    // later: persist user, guard routes, etc
+  const handleLogin = (u: User) => {
+    setUser(u);
   };
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Login onLogin={handleLogin} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login onLogin={handleLogin} />} />
+    </Routes>
   );
 }
 
